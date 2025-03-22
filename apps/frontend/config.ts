1a3dfd611847c05020ee374882b0031da76ce978
AjaykrishnaA <ajaykrishnaatholi@gmail.com>
2025-03-22 19:32:22 +0530

    ws to wss

--- apps/frontend/config.ts
+++ apps/frontend/config.ts
@@ -1,2 +1,2 @@
 export const HTTP_BACKEND = "https://be.sketchpencil.ajaylabs.space";
-export const WS_BACKEND = "ws://ws.sketchpencil.ajaylabs.space"
+export const WS_BACKEND = "wss://ws.sketchpencil.ajaylabs.space"
